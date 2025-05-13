00ec56aa83733eeb457d350cae15591c18e41669
pranavisda1 <pranavisda1@gmail.com>
2025-05-13 12:17:01 +0530

    13/5

--- app/page.tsx
+++ app/page.tsx
@@ -204,7 +204,7 @@ export default function UnchartedLostVoyage() {
             <ul className="list-none text-left space-y-3 mb-6">
               {["You can use Google Search, but avoid AI tools for a more fun experience.",
                 "Use decoding and encoding tools as needed.",
-                "Each puzzle gives a password, take that password and affix it to the end of the URL, do not change gradientaiml.tech, that is constant"].map((rule, index) => (
+                "Each puzzle gives a password, take that password and affix it to the end of the URL, do not change uncharted3.gradientaiml.tech, that is constant"].map((rule, index) => (
                   <li key={index} className="flex items-start text-blue-100">
                     <span className="inline-block mr-2 mt-1 text-purple-400">◆</span>
                     <span>{rule}</span>
